3cc8bb5cdccedd50bb29e81251241a7e4a9cc38f
AlmostRussian <nik.matviets.02@mail.ru>
2023-10-25 21:48:17 +0300

    First commit

--- MainActivity.kt
+++ MainActivity.kt
@@ -1,24 +1,20 @@
-package com.example.p6
+package com.example.p5
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.clickable
 import androidx.compose.material3.Text
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            val count = remember{mutableStateOf(0)}
-
-            Text("Clicks: ${count.value}",
-                fontSize = 28.sp,
-                modifier = Modifier.clickable( onClick = { count.value += 1 })
+            Text(text = "Hello METANIT.COM!",
+                style = TextStyle(
+                    fontSize = 22.sp
+                )
             )
         }
     }
