9ae67a26366c6297b1f58c25535d5166fd949c78
AlmostRussian <nik.matviets.02@mail.ru>
2023-10-25 21:33:22 +0300

    First commit

--- MainActivity.kt
+++ MainActivity.kt
@@ -1,25 +1,24 @@
-package com.example.p6
+package com.example.p6_column
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.clickable
-import androidx.compose.material3.Text
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.sp
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            val count = remember{mutableStateOf(0)}
-
-            Text("Clicks: ${count.value}",
-                fontSize = 28.sp,
-                modifier = Modifier.clickable( onClick = { count.value += 1 })
-            )
+            Column(modifier = Modifier.fillMaxSize(), verticalArrangement = Arrangement.SpaceBetween )
+            {
+                Box(modifier = Modifier.background(Color.Red).fillMaxWidth().height(100.dp))
+                Box(modifier = Modifier.background(Color.Yellow).fillMaxWidth().height(100.dp))
+                Box(modifier = Modifier.background(Color.Green).fillMaxWidth().height(100.dp))
+            }
         }
     }
 }
