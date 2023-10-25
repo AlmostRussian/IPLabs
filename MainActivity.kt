d15de36cdb8f881a77ff93c64e622f7ea84c0102
AlmostRussian <nik.matviets.02@mail.ru>
2023-10-25 21:35:24 +0300

    First commit

--- MainActivity.kt
+++ MainActivity.kt
@@ -1,25 +1,31 @@
-package com.example.p6
+package com.example.p6_kotlininteraction
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
 import androidx.compose.material3.Text
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            val count = remember{mutableStateOf(0)}
+            Column{
+                val createMessage = {hour: Int ->
+                    if (hour > 18) {
+                        "Добрый вечер"
+                    } else if (hour > 10) {
+                        "Добрый день"
+                    } else {
+                        "Доброе утро"
+                    }
+                }
 
-            Text("Clicks: ${count.value}",
-                fontSize = 28.sp,
-                modifier = Modifier.clickable( onClick = { count.value += 1 })
-            )
+                Text(text = createMessage(5), fontSize = 28.sp)
+                Text(text = createMessage(15), fontSize = 28.sp)
+                Text(text = createMessage(19), fontSize = 28.sp)
+            }
         }
     }
 }
