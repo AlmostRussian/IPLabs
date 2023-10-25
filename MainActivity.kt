5d17b62025b451405cb4bdb095dbdaff6e73fc4a
AlmostRussian <nik.matviets.02@mail.ru>
2023-10-25 21:32:07 +0300

    First commit

--- MainActivity.kt
+++ MainActivity.kt
@@ -1,25 +1,28 @@
-package com.example.p6
+package com.example.p6_box
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.clickable
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
 import androidx.compose.material3.Text
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            val count = remember{mutableStateOf(0)}
-
-            Text("Clicks: ${count.value}",
-                fontSize = 28.sp,
-                modifier = Modifier.clickable( onClick = { count.value += 1 })
-            )
+            Box( modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center )
+            {
+                Box(modifier = Modifier.background(Color.Blue).size(300.dp))
+                Box(modifier = Modifier.background(Color.LightGray).size(200.dp))
+                Text("Hello METANIT.COM!", style = TextStyle(fontSize = 22.sp))
+            }
         }
     }
 }
