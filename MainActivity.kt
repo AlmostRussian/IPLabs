afda71d7d3a10929beb6323a2423632180085fd0
AlmostRussian <nik.matviets.02@mail.ru>
2023-10-29 17:27:19 +0300

    First commit

--- MainActivity.kt
+++ MainActivity.kt
@@ -3,23 +3,30 @@ package com.example.p6
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.clickable
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.sp
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            val count = remember{mutableStateOf(0)}
-
-            Text("Clicks: ${count.value}",
-                fontSize = 28.sp,
-                modifier = Modifier.clickable( onClick = { count.value += 1 })
+            val label = remember{mutableStateOf("Click")}
+            Button(onClick = {label.value = "Hello"}){
+                Text(label.value, fontSize = 25.sp)
+            }
+            Button(onClick = {},
+                colors = ButtonDefaults.buttonColors(contentColor = Color.Red)
             )
+            {
+                Text("Click", fontSize = 25.sp)
+            }
         }
     }
 }
+
+
