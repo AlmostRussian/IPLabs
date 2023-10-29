f8b98fc093aa48ad56ad24fc312865cb8ba7ac16
AlmostRussian <nik.matviets.02@mail.ru>
2023-10-29 18:48:00 +0300

    First commit

--- MainActivity.kt
+++ MainActivity.kt
@@ -3,23 +3,53 @@ package com.example.p6
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.clickable
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.selection.selectable
 import androidx.compose.material3.Text
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            val count = remember{mutableStateOf(0)}
 
-            Text("Clicks: ${count.value}",
-                fontSize = 28.sp,
-                modifier = Modifier.clickable( onClick = { count.value += 1 })
-            )
+        setContent {
+            val colors = listOf(
+                ColorData("красный", Color.Red),
+                ColorData("зеленый", Color.Green),
+                ColorData("синий", Color.Blue))
+            val selectedOption = remember { mutableStateOf(colors[0]) }
+            Column(modifier =Modifier.padding(20.dp)) {
+                Box( Modifier.padding(10.dp).size(100.dp).background(color = selectedOption.value.color))
+
+                colors.forEach { colorData ->
+                    val selected = selectedOption.value == colorData
+                    Row(modifier = Modifier.selectable(
+                        selected = selected,
+                        onClick = { selectedOption.value = colorData }),
+                        verticalAlignment = Alignment.CenterVertically
+                    ){
+                        Box(
+                            Modifier.padding(8.dp)
+                                .size(60.dp)
+                                .background(color = colorData.color)
+                                .border(
+                                    width= if(selected){2.dp} else{0.dp},
+                                    color = Color.Black
+                                )
+                        )
+                        Text(text = colorData.title, fontSize = 22.sp)
+                    }
+                }
+            }
         }
     }
 }
+data class ColorData(val title:String, val color: Color)
