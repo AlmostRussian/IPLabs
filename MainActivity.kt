fb5c69d220466065e7b1e7fef8e1adc6308d5ea0
AlmostRussian <nik.matviets.02@mail.ru>
2023-10-29 18:38:35 +0300

    First commit

--- MainActivity.kt
+++ MainActivity.kt
@@ -3,22 +3,27 @@ package com.example.p6
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.clickable
-import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.sp
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.ui.text.input.KeyboardType
 
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            val count = remember{mutableStateOf(0)}
+            val phone = remember { mutableStateOf("") }
 
-            Text("Clicks: ${count.value}",
-                fontSize = 28.sp,
-                modifier = Modifier.clickable( onClick = { count.value += 1 })
+            TextField(
+                phone.value,
+                { phone.value = it },
+                textStyle = TextStyle(fontSize = 28.sp),
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Phone)
             )
         }
     }
