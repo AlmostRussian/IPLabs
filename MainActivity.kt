7f2c6c64da3ad3cc07fc04c37f10b27963ee8b8a
Veaceslav Zolotoi <slavic1233@gmail.com>
2023-10-29 16:18:55 +0200

    Lab7 Progress commit

--- MainActivity.kt
+++ MainActivity.kt
@@ -3,23 +3,97 @@ package com.example.p6
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.clickable
-import androidx.compose.material3.Text
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.ui.Modifier
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.runtime.*
 import androidx.compose.ui.unit.sp
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            val count = remember{mutableStateOf(0)}
+            var progress by remember { mutableStateOf(0.0f) }
+            val scope = rememberCoroutineScope()
 
-            Text("Clicks: ${count.value}",
-                fontSize = 28.sp,
-                modifier = Modifier.clickable( onClick = { count.value += 1 })
-            )
+            Column{
+                Text("Статус: $progress", fontSize = 22.sp)
+                OutlinedButton(
+                    onClick = {
+                        scope.launch {
+                            while (progress < 1f) {
+                                progress += 0.1f
+                                delay(1000L)
+                            }
+                        }
+                    }
+                ) {
+                    Text("Запустить", fontSize = 22.sp)
+                }
+                CircularProgressIndicator(progress = progress)
+            }
+            onClick = {
+                scope.launch {
+                    while (progress < 1f) {
+                        progress += 0.1f
+                        delay(1000L)
+                    }
+                }
+            }
+            Box{ CircularProgressIndicator() }
+
+            var progress by remember { mutableStateOf(0.0f) }
+            val scope = rememberCoroutineScope()
+
+            Column{
+                Text("Статус: $progress", fontSize = 22.sp)
+                OutlinedButton(
+                    modifier = Modifier.padding(20.dp),
+                    onClick = {
+                        scope.launch {
+                            while (progress < 1f) {
+                                progress += 0.1f
+                                delay(1000L)
+                            }
+                        }
+                    }
+                ) {
+                    Text("Запустить", fontSize = 22.sp)
+                }
+
+                LinearProgressIndicator(progress = progress)
+            }
+
+            var progress by remember { mutableStateOf(0.0f) }
+            val scope = rememberCoroutineScope()
+
+            Column{
+                OutlinedButton(
+                    modifier = Modifier.padding(20.dp),
+                    onClick = {
+                        scope.launch {
+                            while (progress < 1f) {
+                                progress += 0.1f
+                                delay(1000L)
+                            }
+                        }
+                    }
+                ) {
+                    Text("Запустить", fontSize = 22.sp)
+                }
+
+                LinearProgressIndicator(
+                    progress = progress,
+                    color = Color(0xFFD32F2F),
+                    backgroundColor = Color(0xFFEF9A9A)
+                )
+            }
+
+            Column{
+                LinearProgressIndicator(modifier = Modifier.padding(20.dp))
+            }
         }
+        super.onCreate(savedInstanceState)
     }
 }
