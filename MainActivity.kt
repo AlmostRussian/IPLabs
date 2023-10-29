85e608de0117267eac8c5224bcf91e276c688ba5
AlmostRussian <nik.matviets.02@mail.ru>
2023-10-29 18:54:03 +0300

    First commit

--- MainActivity.kt
+++ MainActivity.kt
@@ -3,22 +3,28 @@ package com.example.p6
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.clickable
+import androidx.compose.material3.ExtendedFloatingActionButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Delete
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            val count = remember{mutableStateOf(0)}
-
-            Text("Clicks: ${count.value}",
-                fontSize = 28.sp,
-                modifier = Modifier.clickable( onClick = { count.value += 1 })
+            val added = remember{ mutableStateOf(false)}
+            ExtendedFloatingActionButton(
+                icon = {
+                    Icon(
+                        if(added.value) Icons.Filled.Delete else Icons.Filled.Add,
+                        contentDescription = "Добавить"
+                    ) },
+                text = { Text(if(added.value) "Удалить" else "Добавить") },
+                onClick = {added.value = !added.value}
             )
         }
     }
