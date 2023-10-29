9f2bad77a63a1102ae6bce1f0429fd6a8ae681ca
AlmostRussian <nik.matviets.02@mail.ru>
2023-10-29 18:52:03 +0300

    First commit

--- MainActivity.kt
+++ MainActivity.kt
@@ -3,23 +3,36 @@ package com.example.p6
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Row
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconToggleButton
 import androidx.compose.material3.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Info
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.ui.Modifier
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            val count = remember{mutableStateOf(0)}
-
-            Text("Clicks: ${count.value}",
-                fontSize = 28.sp,
-                modifier = Modifier.clickable( onClick = { count.value += 1 })
-            )
+            val checked = remember { mutableStateOf(false) }
+            Row(verticalAlignment = Alignment.CenterVertically){
+                IconToggleButton(checked = checked.value, onCheckedChange = { checked.value = it }) {
+                    Icon(
+                        Icons.Filled.Info,
+                        contentDescription = "Информация о приложении",
+                        tint = if (checked.value) Color(0xFFEC407A) else Color(0xFFB0BEC5)
+                    )
+                }
+                Text(
+                    text = if(checked.value) "Выбрано" else "Не выбрано",
+                    fontSize = 28.sp
+                )
+            }
         }
     }
 }
