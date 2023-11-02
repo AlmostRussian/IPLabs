2197a56e9fde4d6917c3dfd46b4874f7f01b89d5
AlmostRussian <nik.matviets.02@mail.ru>
2023-11-02 14:50:48 +0300

    First commit

--- MainActivity.kt
+++ MainActivity.kt
@@ -4,14 +4,22 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.painter.ColorPainter
+import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.graphics.painter.BitmapPainter
+import androidx.compose.ui.res.imageResource
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Image(painter = ColorPainter(Color.Red), contentDescription = "Красный прямоугольник")
+            Image(
+                painter = BitmapPainter(ImageBitmap.imageResource(R.drawable.forest)),
+                contentDescription = "Зимний лес"
+            )
+            Image(
+                bitmap = ImageBitmap.imageResource(R.drawable.forest),
+                contentDescription = "Зимний лес"
+            )
         }
     }
 }
