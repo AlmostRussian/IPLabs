9149d438b0b9889375ce4b3d8dcad106a6d7639a
AlmostRussian <nik.matviets.02@mail.ru>
2023-11-02 13:39:21 +0300

    First commit

--- MainActivity.kt
+++ MainActivity.kt
@@ -3,23 +3,54 @@ package com.example.p6
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.clickable
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.*
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            val count = remember{mutableStateOf(0)}
-
-            Text("Clicks: ${count.value}",
-                fontSize = 28.sp,
-                modifier = Modifier.clickable( onClick = { count.value += 1 })
-            )
+            val openDialog = remember { mutableStateOf(false) }
+            Button(
+                onClick = { openDialog.value = true }
+            ) {
+                Text("Удалить", fontSize = 22.sp)
+            }
+            if (openDialog.value) {
+                AlertDialog(
+                    onDismissRequest = {
+                        openDialog.value = false
+                    },
+                    title = { Text(text = "Подтверждение действия") },
+                    text = { Text("Вы действительно хотите удалить выбранный элемент?") },
+                    confirmButton = {
+                        Row(
+                            modifier = Modifier.padding(all = 8.dp),
+                            horizontalArrangement = Arrangement.Center
+                        ) {
+                            Button(
+                                modifier = Modifier.weight(1f),
+                                onClick = { openDialog.value = false }
+                            ) {
+                                Text("Удалить")
+                            }
+                            Button(
+                                modifier = Modifier.weight(1f),
+                                onClick = { openDialog.value = false }
+                            ) {
+                                Text("Отмена")
+                            }
+                        }
+                    }
+                )
+            }
         }
     }
 }
