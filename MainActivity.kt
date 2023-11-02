489af035750becf56e12516d39178f49b817d83e
AlmostRussian <nik.matviets.02@mail.ru>
2023-11-02 12:53:30 +0300

    First commit

--- MainActivity.kt
+++ MainActivity.kt
@@ -3,23 +3,31 @@ package com.example.p6
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.clickable
+import androidx.compose.material.*
+import androidx.compose.material3.Button
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.ui.Modifier
+import androidx.compose.runtime.*
 import androidx.compose.ui.unit.sp
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            val count = remember{mutableStateOf(0)}
-
-            Text("Clicks: ${count.value}",
-                fontSize = 28.sp,
-                modifier = Modifier.clickable( onClick = { count.value += 1 })
-            )
+            val scope = rememberCoroutineScope()
+            val snackbarHostState = remember { mutableStateOf(SnackbarHostState()) }
+            Button(
+                onClick = {
+                    scope.launch {
+                        snackbarHostState.value.showSnackbar("Hello")
+                    }
+                }
+            ) {
+                Text("Click", fontSize = 28.sp)
+            }
+            SnackbarHost(snackbarHostState.value)
         }
     }
 }
