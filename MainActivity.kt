2b129d939b5257d12de999ec6fd4741530a83b02
AlmostRussian <nik.matviets.02@mail.ru>
2023-11-02 16:09:32 +0300

    First commit

--- MainActivity.kt
+++ MainActivity.kt
@@ -3,7 +3,6 @@ package com.example.p6
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-
 import com.example.p6.ui.theme.CheckboxinDaRow
 
 class MainActivity : ComponentActivity() {
