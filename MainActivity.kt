c895158876b2f985113fa386312b4267bcf7a44d
AlmostRussian <nik.matviets.02@mail.ru>
2023-11-02 14:07:35 +0300

    First commit

--- MainActivity.kt
+++ MainActivity.kt
@@ -3,23 +3,50 @@ package com.example.p6
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.clickable
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.MoreVert
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.sp
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.unit.DpOffset
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            val count = remember{mutableStateOf(0)}
+            var expanded by remember { mutableStateOf(false) }
+            var selectedOption by remember { mutableStateOf("") }
+            Column{
+                Text("Выбран пункт: $selectedOption")
+                Box {
+                    IconButton(onClick = { expanded = true }) {
+                        Icon(Icons.Default.MoreVert, contentDescription = "Показать меню")
+                    }
+                    DropdownMenu(
+                        expanded = expanded,
+                        onDismissRequest = { expanded = false }
+                    ) {
+                        DropdownMenuItem(
+                            text = { Text("Скопировать") },
+                            onClick = { selectedOption="Copy" },
+                        )
+                        DropdownMenuItem(
+                            text = { Text("Вставить") },
+                            onClick = { selectedOption="Paste" },
+                        )
+                        Divider()
+                        DropdownMenuItem(
+                            text = { Text("Настройки") },
+                            onClick = { selectedOption="Settings" },
+                        )
+                    }
+                }
+            }
 
-            Text("Clicks: ${count.value}",
-                fontSize = 28.sp,
-                modifier = Modifier.clickable( onClick = { count.value += 1 })
-            )
         }
     }
 }
