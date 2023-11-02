79ab281059ff89cb03dfb29df6ddb1824e609152
AlmostRussian <nik.matviets.02@mail.ru>
2023-11-02 13:34:53 +0300

    First commit

--- MainActivity.kt
+++ MainActivity.kt
@@ -3,23 +3,41 @@ package com.example.p6
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.clickable
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            val count = remember{mutableStateOf(0)}
-
-            Text("Clicks: ${count.value}",
-                fontSize = 28.sp,
-                modifier = Modifier.clickable( onClick = { count.value += 1 })
-            )
+            val checkedState = remember { mutableStateOf(false) }
+            val textColor = remember { mutableStateOf(Color.Unspecified) }
+            Column(modifier = Modifier.fillMaxSize().padding(10.dp)){
+                Row (verticalAlignment = Alignment.CenterVertically ){
+                    Text("Красный цвет", fontSize = 22.sp, color = textColor.value)
+                    Switch(
+                        checked = checkedState.value,
+                        onCheckedChange = {
+                            checkedState.value = it
+                            if(checkedState.value) textColor.value = Color(0xFFC62828)
+                            else textColor.value = Color.Unspecified
+                        },
+                        colors = SwitchDefaults.colors(
+                            checkedThumbColor = Color(0xFFC62828),
+                            checkedTrackColor = Color(0xFFE57373),
+                            uncheckedThumbColor = Color(0xFFEF9A9A),
+                            uncheckedTrackColor = Color(0xFFFFEBEE)
+                        )
+                    )
+                }
+            }
         }
     }
 }
