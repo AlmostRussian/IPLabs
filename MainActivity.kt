bbf12e46107a9eb9baa7920706f5ff31df2b1ad9
AlmostRussian <nik.matviets.02@mail.ru>
2023-11-02 14:54:05 +0300

    First commit

--- MainActivity.kt
+++ MainActivity.kt
@@ -3,23 +3,35 @@ package com.example.p6
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.clickable
-import androidx.compose.material3.Text
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Email
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.sp
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.graphics.vector.ImageVector
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            val count = remember{mutableStateOf(0)}
-
-            Text("Clicks: ${count.value}",
-                fontSize = 28.sp,
-                modifier = Modifier.clickable( onClick = { count.value += 1 })
-            )
+            Column {
+                Image(
+                    imageVector = Icons.Filled.Email,
+                    contentDescription = "Значок электронной почты",
+                    modifier = Modifier.size(200.dp, 150.dp)
+                )
+                Image(
+                    imageVector = ImageVector.vectorResource(R.drawable.ic_launcher_background),
+                    contentDescription = "Android"
+                )
+                Image(
+                    imageVector = ImageVector.vectorResource(R.drawable.star),
+                    contentDescription = "Красная звезда"
+                )
+            }
         }
     }
 }
