8f138cf99004e724e7e98816346fea55691a189d
AlmostRussian <nik.matviets.02@mail.ru>
2023-11-02 14:42:34 +0300

    First commit

--- MainActivity.kt
+++ MainActivity.kt
@@ -3,22 +3,26 @@ package com.example.p6
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.clickable
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
 import androidx.compose.material3.Text
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            val count = remember{mutableStateOf(0)}
-
-            Text("Clicks: ${count.value}",
-                fontSize = 28.sp,
-                modifier = Modifier.clickable( onClick = { count.value += 1 })
+            Text(
+                text="Hello METANIT.COM",
+                fontSize= 26.sp,
+                color = colorResource(R.color.textViewFontColor),
+                modifier = Modifier.padding(20.dp)
+                    .fillMaxWidth()
+                    .background(colorResource(R.color.textViewBackColor))
+                    .padding(50.dp)
             )
         }
     }
