dbf9ed4ac1ff065f8ff2994592a0f35e0b200524
AlmostRussian <nik.matviets.02@mail.ru>
2023-11-02 14:08:59 +0300

    First commit

--- MainActivity.kt
+++ MainActivity.kt
@@ -3,23 +3,36 @@ package com.example.p6
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.clickable
-import androidx.compose.material3.Text
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.ui.Modifier
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
 import androidx.compose.ui.unit.sp
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            val count = remember{mutableStateOf(0)}
+            var progress by remember { mutableStateOf(0.0f) }
+            val scope = rememberCoroutineScope()
 
-            Text("Clicks: ${count.value}",
-                fontSize = 28.sp,
-                modifier = Modifier.clickable( onClick = { count.value += 1 })
-            )
+            Column{
+                Text("Статус: $progress", fontSize = 22.sp)
+                OutlinedButton(
+                    onClick = {
+                        scope.launch {
+                            while (progress < 1f) {
+                                progress += 0.1f
+                                delay(1000L)
+                            }
+                        }
+                    }
+                ) {
+                    Text("Запустить", fontSize = 22.sp)
+                }
+                CircularProgressIndicator(progress = progress)
+            }
         }
     }
 }
