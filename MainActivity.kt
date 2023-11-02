b2f97e74cf5ed9a79c28730c216b69c31f594b89
AlmostRussian <nik.matviets.02@mail.ru>
2023-11-02 13:30:19 +0300

    First commit

--- MainActivity.kt
+++ MainActivity.kt
@@ -1,25 +1,85 @@
 package com.example.p6
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ModalNavigationDrawer
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.DrawerState
+import androidx.compose.material3.DrawerValue
+import androidx.compose.material3.ExtendedFloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.ModalDrawerSheet
 import androidx.compose.material3.Text
+import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
+    @SuppressLint("RememberReturnType", "UnusedMaterial3ScaffoldPaddingParameter")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            val count = remember{mutableStateOf(0)}
-
-            Text("Clicks: ${count.value}",
-                fontSize = 28.sp,
-                modifier = Modifier.clickable( onClick = { count.value += 1 })
-            )
+            val scaffoldState = remember {(SnackbarHostState())}
+            val scope = rememberCoroutineScope()
+            val selectedItem = remember{ mutableStateOf("")}
+            val items = listOf("Главная", "Контакты", "О приложении")
+            val drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
+            ModalNavigationDrawer(
+                drawerState = drawerState,
+                drawerContent = {
+                    for (item in items) {
+                        Text(
+                            item,
+                            fontSize = 28.sp,
+                            modifier = Modifier.clickable {
+                                selectedItem.value = item
+                                scope.launch{ drawerState.close() }
+                            }
+                        )
+                    }
+                },
+            ) {
+                Scaffold(
+                    floatingActionButton = {
+                        ExtendedFloatingActionButton(
+                            text = { Text("Show drawer") },
+                            icon = { Icon(Icons.Filled.Add, contentDescription = "") },
+                            onClick = {
+                                scope.launch {
+                                    drawerState.apply {
+                                        if (isClosed) open() else close()
+                                    }
+                                }
+                            }
+                        )
+                    }
+            ){
+                Column{
+                    Button(onClick = {
+                        scope.launch{ drawerState.open() }
+                    }) {
+                        Text("Меню", fontSize = 28.sp)
+                    }
+                    Text("Выбран пункт: ${selectedItem.value}", fontSize = 28.sp)
+                }
+            }
         }
     }
-}
+}}
