fbbf203690c27fbc33591b14dd5a094e0121c903
AlmostRussian <nik.matviets.02@mail.ru>
2023-11-05 20:51:25 +0300

    First commit

--- MainActivity.kt
+++ MainActivity.kt
@@ -5,17 +5,23 @@ import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.android.volley.Request
 import com.android.volley.toolbox.StringRequest
@@ -23,18 +29,18 @@ import com.android.volley.toolbox.Volley
 import com.example.p6.ui.theme.P6Theme
 import org.json.JSONObject
 
-const val API_KEY = "b9bdda6e75f149795d100950231008"
+const val API_KEY = "e11d99d2101c56441ddadc90a43ca726"
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate (savedInstanceState)
+        super.onCreate(savedInstanceState)
         setContent {
             P6Theme {
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                Greeting("Kazan", this)
-            }
+                    Greeting("Kazan", this)
+                }
             }
         }
     }
@@ -43,53 +49,60 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, context: Context) {
-    val state = remember { mutableStateOf("Неопределенный населенный пункт ....") }
-    Column(modifier = Modifier.fillMaxWidth()) {
+    val state = remember {
+        mutableStateOf("Unknown")
+    }
+    Column(
+        modifier = Modifier.fillMaxSize()
+    ) {
         Box(
             modifier = Modifier
                 .fillMaxHeight(0.5f)
                 .fillMaxWidth(),
             contentAlignment = Alignment.Center
         ) {
-            Text(text = "Temp in $name ${state.value} C")
-            Box(
-                modifier = Modifier
-                    .fillMaxHeight()
-                    .fillMaxWidth(),
-                contentAlignment = Alignment.BottomCenter
-            ){
-                Button(
-                    onClick = {
-                        getResult(name, state, context)
-                    },
-                    modifier = Modifier
-                        .padding(5.dp)
-                        .fillMaxWidth()
-                ) {
-                    Text(text = "Обновить")
-                }
+            Text(text = "Temperature in $name: ${state.value} Cº")
+        }
+        Box(
+            modifier = Modifier
+                .fillMaxSize(),
+            contentAlignment = Alignment.BottomCenter
+        ) {
+            Button(onClick = {
+                getData(name, context, state)
+            }, modifier = Modifier.fillMaxWidth().padding(5.dp)) {
+                Text(text = "Refresh")
             }
         }
+
     }
 }
 
-private fun getResult(city: String, state: MutableState<String>, context: Context){
-    val url = "http://api.weatherapi.com/vi/current.json" +
-            "?key=$API_KEY" +
-            "&q = $city" +
-            "&aqi=no"
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    P6Theme {
+
+    }
+}
+
+fun getData(name: String, context: Context, mState: MutableState<String>){
+    val url = "https://api.openweathermap.org/data/2.5/weather" +
+            "?appid=$API_KEY" +
+            "&q=$name" +
+            "&units=metric"
     val queue = Volley.newRequestQueue(context)
     val stringRequest = StringRequest(
         Request.Method.GET,
         url,
         {
+                response->
+            val obj = JSONObject(response)
 
-            response ->
-            val obj=JSONObject(response)
-            state.value=obj.getJSONObject("current").getString("temp_c")
+            val main = obj.getJSONObject("main")
+            mState.value = main.getString("temp")
         },
         {
-            error -> Log.d("Sh1", "Error $error")
         }
     )
     queue.add(stringRequest)
