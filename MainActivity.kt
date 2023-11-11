5ed9f82abc80102b28f9b73dfa88c1162529e89d
AlmostRussian <nik.matviets.02@mail.ru>
2023-11-11 20:24:06 +0300

    First commit

--- MainActivity.kt
+++ MainActivity.kt
@@ -3,11 +3,6 @@ package com.example.p6
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.*
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.p6.bottom_navigation.MainScreen
 import com.example.p6.ui.theme.P6Theme
 
