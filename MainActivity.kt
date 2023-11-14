0dfe3e4d0db810bb2c81085a5483a490538a988d
AlmostRussian <nik.matviets.02@mail.ru>
2023-11-14 18:06:00 +0300

    First commit

--- MainActivity.kt
+++ MainActivity.kt
@@ -5,21 +5,36 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.clickable
 import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.sp
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewmodel.compose.viewModel
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            val count = remember{mutableStateOf(0)}
-
-            Text("Clicks: ${count.value}",
-                fontSize = 28.sp,
-                modifier = Modifier.clickable( onClick = { count.value += 1 })
-            )
+            Screen0()
         }
     }
+}
+
+@Composable
+fun Screen0(myViewModel: MyViewModel = viewModel()) {
+    var i by remember { mutableStateOf(0) }
+    Text(
+        text = "Клики: $i",
+        modifier = Modifier.clickable(onClick = { i++ } )
+    )
+}
+
+class MyViewModel : ViewModel() {
+    val i = mutableStateOf(0)
 }
