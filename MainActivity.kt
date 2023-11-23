535c7a921372bdaa0999e4cd7c86ebe7063688f9
AlmostRussian <nik.matviets.02@mail.ru>
2023-11-23 16:10:46 +0300

    First commit

--- MainActivity.kt
+++ MainActivity.kt
@@ -3,23 +3,48 @@ package com.example.p6
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.clickable
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Text
-import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.toMutableStateList
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.github.javafaker.Faker
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            val count = remember{mutableStateOf(0)}
+            MyScreen()
+        }
+    }
+}
+@Composable
+fun CityItem(text: String) {
+    Text(text = text, fontSize = 20.sp, fontWeight = FontWeight.Bold, modifier = Modifier.fillMaxWidth().border(1.dp,color=
+    Color.Black).padding(5.dp))
+}
+@Composable
+fun MyScreen() {
+    val faker = Faker()
+    val citiz = remember {
+        (1..100).map{ faker.address().city() }.toMutableStateList() //все прекрасно работает
+    }
 
-            Text("Clicks: ${count.value}",
-                fontSize = 28.sp,
-                modifier = Modifier.clickable( onClick = { count.value += 1 })
-            )
+    LazyColumn(verticalArrangement = Arrangement.spacedBy(3.dp), contentPadding = PaddingValues(16.dp),modifier = Modifier.border(3.dp, Color.Gray).padding(10.dp)) {
+
+        items(citiz) { item ->
+            CityItem(text = item)
         }
     }
 }
