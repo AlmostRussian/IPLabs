81345ef2d7500ea73b7215a90992edbceaad9529
AlmostRussian <nik.matviets.02@mail.ru>
2023-11-23 18:52:49 +0300

    First commit

--- MainActivity.kt
+++ MainActivity.kt
@@ -5,27 +5,23 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.lazy.staggeredgrid.LazyVerticalStaggeredGrid
 import androidx.compose.foundation.lazy.staggeredgrid.StaggeredGridCells
 import androidx.compose.foundation.lazy.staggeredgrid.items
 import androidx.compose.foundation.lazy.staggeredgrid.itemsIndexed
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import coil.compose.AsyncImage
 
 class MainActivity : ComponentActivity() {
     val randomSizedPhotos = listOf("...")
@@ -52,74 +48,31 @@ class MainActivity : ComponentActivity() {
                     }
                 }
             }
+            LazyVerticalStaggeredGridSample()
         }
     }
 }
 
-
-@Preview
-@OptIn(ExperimentalFoundationApi::class)
 @Composable
-fun LazyStaggeredGridSnippet() {
+fun LazyVerticalStaggeredGridSample() {
+    val itemsList = (0..5).toList()
+    val itemsIndexedList = listOf("A", "B", "C")
+
+    val itemModifier = Modifier
+        .border(1.dp, Color.Blue)
+        .wrapContentSize()
+
     LazyVerticalStaggeredGrid(
-        columns = StaggeredGridCells.Adaptive(200.dp),
-        verticalItemSpacing = 4.dp,
-        horizontalArrangement = Arrangement.spacedBy(4.dp),
-        content = {
-            items(randomSizedPhotos) { photo ->
-                AsyncImage(
-                    model = photo,
-                    contentScale = ContentScale.Crop,
-                    contentDescription = null,
-                    modifier = Modifier.fillMaxWidth().wrapContentHeight()
-                )
-            }
-        },
-        modifier = Modifier.fillMaxSize()
-    )
+        columns = StaggeredGridCells.Adaptive(200.dp)
+    ) {
+        items(itemsList) {
+            Text("Item is $it", itemModifier.height(80.dp))
+        }
+        item {
+            Text("Single item", itemModifier.height(100.dp))
+        }
+        itemsIndexed(itemsIndexedList) { index, item ->
+            Text("Item at index $index is $item", itemModifier.height(60.dp))
+        }
+    }
 }
-
-
-fun randomSampleImageUrl(
-    seed: Int = (0..100000).random(),
-    width: Int = 300,
-    height: Int = width,
-): String {
-    return "https://picsum.photos/seed/$seed/$width/$height"
-}
-
-@Composable
-fun rememberRandomSampleImageUrl(
-    seed: Int = (0..100000).random(),
-    width: Int = 300,
-    height: Int = width,
-): String = remember { randomSampleImageUrl(seed, width, height) }
-
-private val randomSizedPhotos = listOf(
-    randomSampleImageUrl(width = 1600, height = 900),
-    randomSampleImageUrl(width = 900, height = 1600),
-    randomSampleImageUrl(width = 500, height = 500),
-    randomSampleImageUrl(width = 300, height = 400),
-    randomSampleImageUrl(width = 1600, height = 900),
-    randomSampleImageUrl(width = 500, height = 500),
-    randomSampleImageUrl(width = 1600, height = 900),
-    randomSampleImageUrl(width = 900, height = 1600),
-    randomSampleImageUrl(width = 500, height = 500),
-    randomSampleImageUrl(width = 300, height = 400),
-    randomSampleImageUrl(width = 1600, height = 900),
-    randomSampleImageUrl(width = 500, height = 500),
-    randomSampleImageUrl(width = 900, height = 1600),
-    randomSampleImageUrl(width = 500, height = 500),
-    randomSampleImageUrl(width = 300, height = 400),
-    randomSampleImageUrl(width = 1600, height = 900),
-    randomSampleImageUrl(width = 500, height = 500),
-    randomSampleImageUrl(width = 500, height = 500),
-    randomSampleImageUrl(width = 300, height = 400),
-    randomSampleImageUrl(width = 1600, height = 900),
-    randomSampleImageUrl(width = 500, height = 500),
-    randomSampleImageUrl(width = 900, height = 1600),
-    randomSampleImageUrl(width = 500, height = 500),
-    randomSampleImageUrl(width = 300, height = 400),
-    randomSampleImageUrl(width = 1600, height = 900),
-    randomSampleImageUrl(width = 500, height = 500),
-)
